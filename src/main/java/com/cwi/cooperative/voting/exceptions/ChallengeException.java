683fbec66a19fc2b88989fbfa1304d733286e091
Ricardo Spinoza <ricardospinoza@gmail.com>
2021-08-11 21:17:14 -0300

    Remoção do serialVersionUID sugestão codereview

--- src/main/java/com/cwi/cooperative/voting/exceptions/ChallengeException.java
+++ src/main/java/com/cwi/cooperative/voting/exceptions/ChallengeException.java
@@ -3,12 +3,11 @@ package com.cwi.cooperative.voting.exceptions;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@SuppressWarnings("serial")
 @Data
 @NoArgsConstructor
 public class ChallengeException extends RuntimeException {
 
-	private static final long serialVersionUID = 6012044635111833568L;
-	
 	public ChallengeException(String message) {
 		super(message);
 	}
