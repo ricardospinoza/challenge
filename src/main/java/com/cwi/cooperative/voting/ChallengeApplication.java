5212c51833f0979788311a88b277edabcfbaa06b
Ricardo Spinoza <ricardospinoza@gmail.com>
2021-08-12 14:20:43 -0300

    JavaDoc e remoção linhas em branco

--- src/main/java/com/cwi/cooperative/voting/ChallengeApplication.java
+++ src/main/java/com/cwi/cooperative/voting/ChallengeApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Tratamento de exceções do sistema
+ * @author ricardo.spinoza
+ *
+ */
 @SpringBootApplication
 @EnableFeignClients
 public class ChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
-
-
 }
